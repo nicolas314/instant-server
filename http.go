c274e7dc7ead847759bfe5df3f954bf112acb578
devil <devil@fido.lan>
2024-02-14 21:10:49 +0100

    Added logging on stdout

--- http.go
+++ http.go
@@ -6,12 +6,21 @@ import (
     "net/http"
     "os"
 )
+
+func logHandler(fs http.Handler) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        log.Printf("[%v][%v]\n", r.RemoteAddr, r.URL)
+        fs.ServeHTTP(w, r)
+    }
+}
+
 func main() {
     addr := ":8080"
     if len(os.Args)>1 {
         addr = os.Args[1]
     }
     log.Println("Listening on "+addr)
-    panic(http.ListenAndServe(addr, http.FileServer(http.Dir("."))))
+    fileServer := http.FileServer(http.Dir("."))
+    http.ListenAndServe(addr, logHandler(fileServer))
 }
 
